7829e339ded2ba0e253505cb425c4fc11634ac3d
shellbell22 <morris.shelley@gmail.com>
2016-07-03 15:24:25 -0400

    through lesson2-4v2

--- js/app.js
+++ js/app.js
@@ -8,11 +8,17 @@ $(document).ready(function() {
 		$('.ryu-still').show();
   	})
  	.mousedown(function() {
-   	 	// play hadouken sound
+   	 	playHadouken();
   		$('.ryu-ready').hide();
   		$('.ryu-throwing').show();
-  		$('.hadouken').show();
-  // animate hadouken to the right of the screen
+  		$('.hadouken').finish().show().animate(
+  			{'left': '1020px'},
+  			500,
+  			function() {
+   			 	$(this).hide();
+    			$(this).css('left', '520px');
+  			}
+		);
 	})
   	.mouseup(function() {
     	$('.ryu-throwing').hide();
@@ -20,3 +26,9 @@ $(document).ready(function() {
   	});
 
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
