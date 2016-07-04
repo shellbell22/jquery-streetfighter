a66c5c64bcdfdc0f3fd7205a73ea4f3f4d95d92b
shellbell22 <morris.shelley@gmail.com>
2016-07-03 21:20:12 -0400

    with keys and text

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
   	})
  	.mouseleave(function() {
-    	$('.ryu-ready').hide();
-		$('.ryu-still').show();
+    //	$('.ryu-ready').hide();
+	//	$('.ryu-still').show();
   	})
  	.mousedown(function() {
    	 	playHadouken();
@@ -23,9 +23,39 @@ $(document).ready(function() {
   	.mouseup(function() {
     	$('.ryu-throwing').hide();
   		$('.ryu-ready').show();
-  	});
+  	}); 
+
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
+			//alert('x is pressed');
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(e){
+		if(e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	})
+
+
+
 
 });
+
+var loopcount = 1;
+function myloop () {
+	setTimeout(function(){ 
+		$('.text').fadeOut(1000);
+		$('#text' + loopcount).delay(1000).fadeIn(1000);
+		loopcount++;
+		if (loopcount > 2) loopcount = 1;
+		myloop();
+		}, 3000);
+}
+myloop();
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
